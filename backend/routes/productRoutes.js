ade190005e94d888f8c506e3c42979cdb2a04d09
MahadevBalla <mahadevballa36@gmail.com>
2025-04-20 03:13:52 +0530

    modified products' deletion route in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,7 +10,7 @@ import { validateProduct } from "../middlewares/validateProduct.js";
 
 const router = express.Router();
 
-// GET /api/products - Get all products
+// GET /api/v1/products - Get all products
 router.get("/", getProducts);
 
 // POST /api/products - Create new product
@@ -19,6 +19,9 @@ router.post("/", validateProduct, createProduct);
 // DELETE route for purchases
 router.post("/purchase", purchaseProduct);
 
+// DELETE route for products
+router.post("/delete/:productName", purchaseProduct);
+
 //Update a product
-router.patch("/:productName", updateProduct);
+router.patch("/update/:productName", updateProduct);
 export default router;
